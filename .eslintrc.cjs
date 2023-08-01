4de3e6d12bcffcfbeeb6c2d94f1003902e61c91c
Chuck Okoroji <dandychux@gmail.com>
2023-08-01 01:29:24 -0400

    updated eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,6 +26,8 @@ const config = {
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-empty-interface": "off",
   },
 };
 
